6ce20b89d3dd1582ce418045dd1d65d0280de0ee
Joan <joan.gawelda@gmail.com>
2018-04-20 15:02:05 +0300

    Rename

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,8 +6,8 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1 className="box-layout__title">Blogger</h1>
-      <p>Tag line for app.</p>
+      <h1 className="box-layout__title">DiarGo</h1>
+      <p>Your personal web diary</p>
       <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
